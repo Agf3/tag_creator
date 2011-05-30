cf8eb7584251595ac91a5166e6dea1eebd404e9d
Eli Goldner <eg38@njit.edu>
2011-05-30 22:17:56 +0000

    Tag Generator

--- tag.php
+++ tag.php
@@ -36,6 +36,9 @@ function exception_tester($tag, $attributes, $content){
 		if(!is_string($content)){
 			throw  new Exception("Content must be strings", 111);
 		}
+		if (!html5TagValidate($tag)){
+			throw new Exception("Invalid HTML5 tag.", 123);
+		}
 }
 
 //parse exception details and returns an array that can be written to a log
@@ -59,6 +62,32 @@ function classify($code){
 	}
 	return $classify;
 }
+
+
+/*
+*This function will check if the given tag is a valid tag in HTML5.
+*/
+function html5TagValidate($tag){
+
+                html5ValidTags = array ("!--", "!DOCTYPE", "a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdo",
+                 "blockquote", "body", "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", "command", "datalist", "dd",
+                 "del", "details", "dfn", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2",
+                 "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "keygen", "kbd",
+                 "label", "legend", "li", "link", "map", "mark", "menu", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup",
+                 "option", "output", "p", "param", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select",
+                 "small", "source", "span", "strong", "style", "sub", "summary", "sup", "table", "tbody", "td", "textarea", "tfoot", "th",
+                 "thead", "time", "title", "tr", "ul", "var", "video", "wbr"  );
+
+
+                if (!in_array($tag, $html5ValidTags)){
+                        return false;
+                }else{
+                        return true;
+                }
+
+}
+
+
 ?>
 
 
