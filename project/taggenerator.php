8a760338c07301719dacf723b1eff639c00b070f
ct86 <ct86@njit.edu>
2011-06-10 19:39:02 +0000

    Integrated

--- project/taggenerator.php
+++ project/taggenerator.php
@@ -106,7 +106,7 @@ class NonStringException extends Exception{
 
 //is this what is meant by automatically starting objects ?
 $tag = "a";
-$attributes = array("id" => "myid", "href" => "http://www.http://ruxtongroup.com", "class" => "myfirstclass mysecondclass");
+$attributes = array("id" => "myid", "href" => "http://www.ruxtongroup.com", "class" => "myfirstclass mysecondclass");
 $content = "Test tag";
 new TagGenerator($tag, $attributes, $content);
 echo "Done";
